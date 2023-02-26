9a1fd466936f09ca258ca2163f19fe99a4c69f43
dustyou <1172073068@qq.com>
2023-02-26 18:26:44 +0800

    更新

--- front/hello-vue/src/components/DemoTest.vue
+++ front/hello-vue/src/components/DemoTest.vue
@@ -2,7 +2,7 @@
 <!--    <h1>demo01test</h1>-->
     <h3>在浙之滨 艺友云谈</h3>
     <h3>浙大&国美联谊交友活动方案</h3>
-<!--    <img alt="Vue logo" src="../assets/logo.png">-->
+    <img alt="Vue logo" src="../assets/welcome.jpg">
 </template>
 
 <script>
